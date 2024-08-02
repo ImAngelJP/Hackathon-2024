75328d0843868d252ac1f35d3c847d4e64c2b52e
Parada Estrada <70494@bi.com.gt>
2024-08-01 18:55:26 -0600

    Carousel de blogs

--- carrusel.js
+++ carrusel.js
@@ -22,7 +22,34 @@ const prevSlide = () => {
     showSlides(--slideIndex);
 }
 
+let slideBlogIndex = 0;
+
+const showSlidesBlog = (n) => {
+    const slides = document.querySelectorAll(".carousel-blog-slide");
+    if (n >= slides.length) {
+        slideBlogIndex = 0;
+    }
+    if (n < 0) {
+        slideBlogIndex = slides.length - 1;
+    }
+    slides.forEach((slide, index) => {
+        slide.style.display = "none";
+    });
+    slides[slideBlogIndex].style.display = "block";
+}
+
+const nextSlideBlog = () => {
+    showSlidesBlog(++slideBlogIndex);
+}
+
+const prevSlideBlog = () => {
+    showSlidesBlog(--slideBlogIndex);
+}
+
 document.querySelector(".carousel-next").addEventListener("click", nextSlide);
 document.querySelector(".carousel-prev").addEventListener("click", prevSlide);
+document.querySelector(".carousel-blog-next").addEventListener("click", nextSlideBlog);
+document.querySelector(".carousel-blog-prev").addEventListener("click", prevSlideBlog);
 
 showSlides(slideIndex);
+showSlidesBlog(slideBlogIndex);
